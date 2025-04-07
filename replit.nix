2e1d9432a30cebc2922996ff315bd2c4e9721b14
Marcos Fonseca <61661813+marcosmarf27@users.noreply.github.com>
2025-04-07 15:54:06 +0000

    versao estavel

--- replit.nix
+++ replit.nix
@@ -1,11 +1,5 @@
 {pkgs}: {
   deps = [
-    pkgs.pango
-    pkgs.harfbuzz
-    pkgs.glib
-    pkgs.ghostscript
-    pkgs.fontconfig
-    pkgs.freetype
     pkgs.jq
     pkgs.glibcLocales
     pkgs.postgresql
